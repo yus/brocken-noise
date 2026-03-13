2a838babcb87bcad8343c6c5cce7369307e24814
yus <swiftcase@gmail.com>
2026-03-13 13:39:51 +0800

    Update main.js

--- CA/js/main.js
+++ CA/js/main.js
@@ -906,12 +906,6 @@
         
         DEBUG.log('Starting animation loop');
         animate(0);
-        // Force a redraw every frame regardless of CA update
-        function forceRender() {
-            renderer.render(time, ca.calculateMetrics());
-            requestAnimationFrame(forceRender);
-        }
-        forceRender();
     }
     
     // Start
